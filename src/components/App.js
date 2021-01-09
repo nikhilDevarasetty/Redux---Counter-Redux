84d7becf089b1f1518fcd38689f5a89479df1635
nikhilDevarasetty <nicholus158@gmail.com>
2021-01-09 11:27:25 +0530

    testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
+import { store, counterSlice } from "../store";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const incre = () => {
+    store.dispatch(counterSlice.actions.increment());
+    setCounter(store.getState());
+  };
+  const decre = () => {
+    store.dispatch(counterSlice.actions.decrement());
+    setCounter(store.getState());
+  };
 
   return (
-    <div id='main'>
-      
+    <div id="main">
+      <div data-testid="counter">{counter}</div>
+      <button onClick={incre}>increment</button>
+      <button onClick={decre}>decrement</button>
     </div>
   );
 }
