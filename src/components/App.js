313aaa4d8d6a50ff51fa02852256bac41ff3e43f
nikhilDevarasetty <nicholus158@gmail.com>
2021-01-17 00:20:07 +0530

    testing

--- src/components/App.js
+++ src/components/App.js
@@ -43,7 +43,7 @@ function App() {
 
   return (
     <div id="main">
-      <h3 data-testid="counter">Count: {store.counter}</h3>
+      <h3 data-testid="counter">{store.counter}</h3>
       <button onClick={inc}>+</button>
       <br />
       <br />
